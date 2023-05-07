76fe0f59925ddb397f3555eaf0f02d1225403000
Valeria Mora <valeria.mora2799@gmail.com>
2023-05-06 20:25:45 -0600

    Comentando y agregando nueva instancia
    Signed-off-by: Valeria Mora <valeria.mora2799@gmail.com>

--- Templates/stackTemplate.cpp
+++ Templates/stackTemplate.cpp
@@ -29,16 +29,32 @@ OTROS ACUERDOS EN EL SOFTWARE.
 #include <stdexcept>
 #include <functional>
 
+/**
+ * @brief Template con una implementación de stack
+ * 
+ * @tparam T El tipo de los elementos en la pila.
+*/
 template<typename T>
 class Stack {
 private:
   std::vector<T> data_;
 
 public:
+  /**
+   * @brief Función que permite agregar un elemento al final del vector.
+   * 
+   * @param value El valor a agregar a la pila.
+  */
   void push(T value) {
     data_.push_back(value);
   }
 
+  /**
+   * @brief Función que se encarga de eliminar el último elemento de la pila.
+   * 
+   * @return El valor en la parte superior de la pila.
+   * @throws std::out_of_range Si la pila está vacía.
+  */
   T pop() {
     if (data_.empty()) {
       throw std::out_of_range("Stack is empty");
@@ -48,42 +64,116 @@ public:
     return value;
   }
 
+  /**
+   * @brief Función que se encarga de limpiar la pila.
+  */
   void clear() {
     data_.clear();
   }
 
+  /**
+   * @brief Función que se encarga de verificar si la pila está vacía.
+   * 
+   * @return `true` si la pila está vacía, `false` en caso contrario.
+  */
   bool empty() const {
     return data_.empty();
   }
 
+  /**
+   * @brief Función que obtiene el número de elementos de la pila.
+   * 
+   * @return El número de elementos en la pila.
+  */
   std::size_t size() const {
     return data_.size();
   }
 
+  /**
+   * @brief Función que permite alicar una función a cada elemento en la pila.
+   * 
+   * @param func La función a aplicar a cada elemento.
+  */
   void foreach(const std::function<void(T&)>& func) {
     std::for_each(data_.begin(), data_.end(), func);
   }
 };
 
+/**
+ * @brief Función principal.
+ * 
+ * @return 0 en caso de éxito.
+*/
 int main() {
-  Stack<int> s;
+  Stack<int> s; /*se crea una instancia de la clase Stack*/
+
+  /*Se agregan tres valores enteros a la pila con push: */
   s.push(2021);
   s.push(2054);
   s.push(6524);
 
+  /* Se imprime el tamaño de la pila usando el método size.*/
   std::cout << "Stack size: " << s.size() << std::endl;
 
+  /* Se llama al método foreach de la pila, que recorre todos los elementos de la pila y 
+  los imprime en la pantalla utilizando una función lambda.*/
   s.foreach([](int& value) {
     std::cout << "Value: " << value << std::endl;
   });
 
+  /* Ciclo que extrae los valores de la pila utilizando el método pop y 
+  los imprime en la pantalla. Este bucle se ejecuta hasta que la pila este vacía.*/
   try {
     while (!s.empty()) {
       int value = s.pop();
       std::cout << "Popped value: " << value << std::endl;
     }
+    /*Se imprime el tamaño de la pila*/
     std::cout << "Stack size: " << s.size() << std::endl;
-  } catch (const std::exception& e) {
+  } 
+  /*Si la pila está vacía y se intenta retirar un elemento, se lanzará una excepción de tipo 
+  std::out_of_range que se captura en un bloque catch. La excepción se imprime en la pantalla utilizando cerr*/
+  catch (const std::exception& e) {
+    std::cerr << "Exception: " << e.what() << std::endl;
+  }
+  std::cout << std::endl;
+
+
+  /*Nueva instancia*/
+  Stack<std::string> sn; 
+
+  /*Se agregan tres strings a la pila con push: */
+  sn.push("Valeria");
+  sn.push("Diego");
+  sn.push("Carlos");
+  sn.push("Karla");
+
+  /* Se imprime el tamaño de la pila usando el método size.*/
+  std::cout << "Stack size: " << sn.size() << std::endl;
+
+  /* Se llama al método foreach de la pila, que recorre todos los elementos de la pila y 
+  los imprime en la pantalla utilizando una función lambda.*/
+  sn.foreach([](std::string& value) {
+    std::cout << "Name: " << value << std::endl;
+  });
+
+  /* Ciclo que extrae los valores de la pila utilizando el método pop y 
+  los imprime en la pantalla. Este bucle se ejecuta hasta que la pila este vacía.*/
+  try {
+    while (!sn.empty()) {
+      std::string value = sn.pop();
+      std::cout << "Popped value: " << value << std::endl;
+    }
+    /*Se imprime el tamaño de la pila*/
+    std::cout << "Stack size: " << sn.size() << std::endl;
+
+    /*Se llama al método pop() y se trata de eliminar un valor de la pila. 
+    Como la pila está vacía, entonces dispararía la excepción out_of_range*/
+    std::string value = sn.pop();
+  } 
+  /*Si la pila está vacía y se intenta retirar un elemento, se lanzará una excepción de tipo 
+  std::out_of_range que se captura en un bloque catch. La excepción se imprime en la pantalla utilizando cerr*/
+  catch (const std::exception& e) {
     std::cerr << "Exception: " << e.what() << std::endl;
   }
 
